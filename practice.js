b6cc18b5576cdb6a9c567a873eb3caec286f7ec7
unknown <umairnoonari98@gmail.com>
2023-08-09 14:12:52 +0500

    Check a Number is prime or not

---
+++ practice.js
@@ -0,0 +1,35 @@
+function prime(num)
+{
+    const flag=true
+    for(let i=2;i<num;i++)
+    {
+        if(num%i!=0)
+        {
+            flag=true
+        }
+    }
+    if(flag===true)
+    {
+        console.log("Prime")
+    }
+}
+function Prime(num)
+{
+    let flag=true
+    for (let index = 2; index < num; index++) {
+        if(num%index===0)
+        {
+            flag=false
+            break
+        }
+    }
+    if(flag===true)
+    {
+        console.log("Prime")
+    }
+    else
+    {
+        console.log("not Prime")
+    }
+}
+Prime(7)
